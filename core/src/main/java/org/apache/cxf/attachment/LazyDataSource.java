6274a9ef92b6a98471b55539a32cb87f591983eb
AapooYYY <75163856+AapooYYY@users.noreply.github.com>
2021-04-10 16:30:44 +0300

    Added new class CustomDataSource
    New feature first version

--- core/src/main/java/org/apache/cxf/attachment/LazyDataSource.java
+++ core/src/main/java/org/apache/cxf/attachment/LazyDataSource.java
@@ -99,3 +99,31 @@ public class LazyDataSource implements DataSource {
         this.dataSource = dataSource;
     }
 }
+
+
+public class CustomDataSource implements DataSource {
+
+    private String name;
+    private String id;
+
+    public SimpleDataSource(String nameParam, String idParam) throws IOException {
+        this.name = nameParam;
+        this.id = idParam;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newname) {
+		    this.name = newname;
+    }
+	
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String newid) {
+		    this.id = newid;
+    }
+}
