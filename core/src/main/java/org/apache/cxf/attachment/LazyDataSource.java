7c5939a43b5b1286c58444bae1eefdafd2908485
AapooYYY <75163856+AapooYYY@users.noreply.github.com>
2021-04-10 17:06:50 +0300

    CustomDataSourceRemoved

--- core/src/main/java/org/apache/cxf/attachment/LazyDataSource.java
+++ core/src/main/java/org/apache/cxf/attachment/LazyDataSource.java
@@ -99,31 +99,3 @@ public class LazyDataSource implements DataSource {
         this.dataSource = dataSource;
     }
 }
-
-
-public class CustomDataSource implements DataSource {
-
-    private String name;
-    private String id;
-
-    public CustomDataSource(String nameParam, String idParam) {
-        this.name = nameParam;
-        this.id = idParam;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String newname) {
-        this.name = newname;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String newid) {
-        this.id = newid;
-    }
-}
