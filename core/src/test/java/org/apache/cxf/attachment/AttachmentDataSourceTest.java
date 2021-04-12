44de33225315d5068e786c4c9ff2cbd7deaf0efb
JaakkoTervonen <78486122+JaakkoTervonen@users.noreply.github.com>
2021-04-12 15:35:59 +0300

    Update AttachmentDataSourceTest.java
    imported assertEquals

--- core/src/test/java/org/apache/cxf/attachment/AttachmentDataSourceTest.java
+++ core/src/test/java/org/apache/cxf/attachment/AttachmentDataSourceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
