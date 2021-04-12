16658991ed2e43fa9b40c67df44e42e3b23758fb
AapooYYY <75163856+AapooYYY@users.noreply.github.com>
2021-04-12 14:08:24 +0300

    getName test

--- core/src/test/java/org/apache/cxf/attachment/AttachmentDataSourceTest.java
+++ core/src/test/java/org/apache/cxf/attachment/AttachmentDataSourceTest.java
@@ -1,1 +1,23 @@
+package org.apache.cxf.attachment;
 
+import java.util.Collections;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class AttachmentDataSourceTest {
+
+    private static final String ctParam = "ctParam";
+    private static final InputStream InParam = "inputstream";
+  
+    @Test
+    public void testGetName() {
+        DataSource ds = new LazyDataSource(ctParam, inParam);
+        assertEquals("ctParam",ds.getName());
+    }
+}
