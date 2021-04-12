829c17713610107cd2eba71bca091f9b12ebd6c5
AapooYYY <75163856+AapooYYY@users.noreply.github.com>
2021-04-12 13:31:03 +0300

    createAttachmentDataSource from private to public

--- core/src/main/java/org/apache/cxf/attachment/AttachmentUtil.java
+++ core/src/main/java/org/apache/cxf/attachment/AttachmentUtil.java
@@ -414,7 +414,7 @@ public final class AttachmentUtil {
             headers.remove("Content-Transfer-Encoding");
         }
         DataSource source = new AttachmentDataSource(ct, ins);
-        DataSource source2 = new AttachmentDataSource(ct, ins);
+        DataSource source2 = createAttachmentDataSource(ct, ins);
         if (!StringUtils.isEmpty(fileName)) {
             ((AttachmentDataSource)source).setName(FileUtils.stripPath(fileName));
         }
@@ -564,7 +564,7 @@ public final class AttachmentUtil {
         return new LazyDataSource(contentId, atts);
     }
     
-    private static DataSource createAttachmentDataSource(String ctParam, InputStream inParam) {
+    public static DataSource createAttachmentDataSource(String ctParam, InputStream inParam) {
         return new AttachmentDataSource(ctParam, inParam);
     }
 
